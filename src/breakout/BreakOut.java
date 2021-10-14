9fe5515927c7b9aaad9ecdb931d085dd8bea3d0e
darzavaliuk <dar.zavaliuk@gmail.com>
2021-10-14 09:31:27 +0300

    main class BreakOut.java

---
+++ src/breakout/BreakOut.java
@@ -0,0 +1,12 @@
+package breakout;
+import javax.swing.JFrame;
+
+public class BreakOut extends JFrame {
+    public BreakOut(){
+        super("BreakOut");
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
