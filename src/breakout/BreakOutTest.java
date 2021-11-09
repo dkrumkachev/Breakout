d3ea38cb0d409ea8bb74d1027d27d6f63eb48383
Daria <smilikk1111@gmail.com>
2021-11-09 09:11:46 +0300

    Test for class Java

---
+++ src/breakout/BreakOutTest.java
@@ -0,0 +1,77 @@
+package breakout;
+
+import org.junit.Test;
+import org.junit.jupiter.api.DisplayName;
+
+import javax.swing.*;
+
+import static org.junit.Assert.assertEquals;
+
+public class BreakOutTest{
+
+    @Test
+    @DisplayName("Order Matrix for 3 cols 3 rows for order matrix same elements")
+    public void test1(){
+        boolean expectedResult = true;
+        boolean actualResult;
+        JInternalFrame game = new JInternalFrame();
+        game.pack();
+        game.setResizable(false);
+        try{
+            game.setResizable(false);
+        }catch (Exception ignored){
+        }
+        actualResult = true;
+        assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    @DisplayName("Order Matrix for 3 cols 3 rows for order matrix same elements")
+    public void test2(){
+        boolean expectedResult = true;
+        boolean actualResult;
+        JInternalFrame game = new JInternalFrame();
+        game.pack();
+        game.setVisible(true);
+        try{
+            game.setVisible(true);
+        }catch (Exception ignored){
+        }
+        actualResult = true;
+        assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    @DisplayName("Order Matrix for 3 cols 3 rows for order matrix same elements")
+    public void main(){
+        boolean expectedResult = true;
+        boolean actualResult;
+        JInternalFrame game = new JInternalFrame();
+        game.pack();
+        game.setVisible(true);
+        try{
+            game.setVisible(true);
+        }catch (Exception ignored){
+        }
+        actualResult = true;
+        assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    @DisplayName("Order Matrix for 3 cols 3 rows for order matrix same elements")
+    public void test4(){
+        boolean expectedResult = true;
+        boolean actualResult;
+        JInternalFrame game = new JInternalFrame();
+        game.pack();
+        game.setVisible(true);
+        try{
+            new BreakOut(){};
+        }catch (Exception ignored){
+        }
+        actualResult = true;
+        assertEquals(expectedResult, actualResult);
+    }
+
+
+}
