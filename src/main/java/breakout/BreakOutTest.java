7e0029887515e49a720ca79e49caea36d4b2b777
Dmitriy <d.krumkachev@gmail.com>
2021-11-17 22:26:32 +0300

    maven

--- src/main/java/breakout/BreakOutTest.java
+++ src/main/java/breakout/BreakOutTest.java
@@ -1,11 +1,11 @@
-package breakout;
+package main.java.breakout;
 
-import org.junit.Test;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 import javax.swing.*;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class BreakOutTest{
 
