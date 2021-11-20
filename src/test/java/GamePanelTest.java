f708e407b3d2bb2c09ae9da1687e211340679b28
Daria <dar.zavaliuk@gmail.com>
2021-11-20 19:20:43 +0300

    test

--- src/test/java/GamePanelTest.java
+++ src/test/java/GamePanelTest.java
@@ -28,20 +28,6 @@ public class GamePanelTest {
         assertEquals(expectedResult, actualResult);
     }
 
-
-    @Test
-    @DisplayName("Test Function Refresh")
-    public void testRefresh(){
-        boolean expectedResult;
-        try{
-            new refresh();
-        }catch (Exception ignored){
-        }
-        expectedResult = true;
-        boolean actualResult = true;
-        assertEquals(expectedResult, actualResult);
-    }
-
     @Test
     @DisplayName("Test Oval for 1x4")
     public void testOval1x4(){
@@ -50,66 +36,6 @@ public class GamePanelTest {
         Graphics g = null;
         try {
             g.fillOval(1, 4, 7, 7);
-        }catch (Exception ignored){
-
-        }
-        boolean actualResult = true;
-        assertEquals(expectedResult, actualResult);
-    }
-
-    @Test
-    @DisplayName("Test Start Game")
-    public void testStartGame(){
-        boolean expectedResult = true;
-        new GamePanel();
-        Graphics g = null;
-        try {
-            drawStartGame(Objects.requireNonNull(g));
-        }catch (Exception ignored){
-
-        }
-        boolean actualResult = true;
-        assertEquals(expectedResult, actualResult);
-    }
-
-    @Test
-    @DisplayName("Test Options")
-    public void testOptions(){
-        boolean expectedResult = true;
-        new GamePanel();
-        Graphics g = null;
-        try {
-            drawOptions(g);
-        }catch (Exception ignored){
-
-        }
-        boolean actualResult = true;
-        assertEquals(expectedResult, actualResult);
-    }
-
-    @Test
-    @DisplayName("Test End Panel")
-    public void testEndPanel(){
-        boolean expectedResult = true;
-        new GamePanel();
-        Graphics g = null;
-        try {
-            win(g);
-        }catch (Exception ignored){
-
-        }
-        boolean actualResult = true;
-        assertEquals(expectedResult, actualResult);
-    }
-
-    @Test
-    @DisplayName("Test Game Over")
-    public void testGameOver(){
-        boolean expectedResult = true;
-        new GamePanel();
-        Graphics g = null;
-        try {
-            showGameOver(g);
         }catch (Exception ignored){
 
         }
@@ -152,42 +78,6 @@ public class GamePanelTest {
         }
         boolean actualResult = true;
         assertEquals(expectedResult, actualResult);
-    }
-
-    private void showGameOver(Graphics g) {
-        g.setColor(Color.orange);
-        g.setFont(new Font("Times new roman", Font.PLAIN, 40));
-        g.drawString("GAME OVER", 453 / 2 - 170, 456 / 2 + 10);
-        g.setColor(Color.GREEN);
-        g.setFont(new Font("Times new roman", Font.BOLD, 40));
-        g.drawString("SCORE : ", 466 / 2 - 150, 543 / 2 + 60);
-    }
-
-    private void win(Graphics g) {
-        g.setColor(Color.ORANGE);
-        g.setFont(new Font("Times new roman", Font.ITALIC, 40));
-        g.drawString("You Have Cleared The Game.", 14 / 2 - 260, 34 / 2 - 120);
-        g.setColor(Color.GREEN);
-    }
-
-
-    private void drawOptions(Graphics g) {
-        g.setColor(Color.BLACK);
-        g.setFont(new Font("Times new roman", Font.ITALIC, 42));
-        g.drawString("Press [ESC] to exit the game", 34 / 2 - 260, 432 / 2 + 120);
-        g.drawString("Press [R] to restart the game", 54 / 2 - 260, 432 / 2 + 170);
-        g.setColor(Color.GREEN);
-        g.setFont(new Font("Times new roman", Font.BOLD, 42));
-        g.drawString("SCORE : " , 56 / 2 - 150, 653 / 2 - 70);
-    }
-
-    private void drawStartGame(Graphics g) {
-        g.setColor(Color.black);
-        g.setFont(new Font("Times new roman", Font.ITALIC, 42));
-        g.drawString("Press [Space] to start the game", 456 / 2 - 280, 65 / 2 + 30);
-    }
-
-    private static class refresh {
     }
 
 }
