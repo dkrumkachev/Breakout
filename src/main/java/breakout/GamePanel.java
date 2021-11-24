9f878e6497bad8759b5aba34389d1b4dd754c6f0
Dmitriy <d.krumkachev@gmail.com>
2021-11-24 19:47:45 +0300

    game panel test

--- src/main/java/breakout/GamePanel.java
+++ src/main/java/breakout/GamePanel.java
@@ -1,11 +1,260 @@
 package breakout;
 
+import java.util.Scanner;
+
 public class GamePanel {
     final static int numberColumns = 3;
     final static int numberLines = 3;
-    public static void main(String[] args) {
-
+    public static Scanner in;
+    public static void outputTask() {
+        System.out.println("Программа позволяет ввести два натуральных числа m и n (m>n)\n" +
+                "казывает на экране правило деления «лесенкой» числа m на число n.");
+                System.out.println("Введите число 1: ");
     }
+
+    public static int checkNumber() {
+        in = new Scanner(System.in);
+        boolean isInCorrect;
+        int number = 0;
+        do {
+            isInCorrect = false;
+            try {
+                number = Integer.parseInt(in.nextLine());
+            } catch (Exception e) {
+                System.out.println("Проверьте корректность введенных данных!");
+                isInCorrect = true;
+            }
+            if ((!isInCorrect) && (number < 0))
+            {
+                System.out.println("Проверьте корректность введенных данных!");
+                System.out.println("Введите натуральное число.");
+                isInCorrect = true;
+            }
+        } while (isInCorrect);
+        in.close();
+        return number;
+    }
+
+    public static int checkNumberSecondNumber(int firstNumber) {
+        System.out.println("Введите второе число: ");
+        int secondNumber = checkNumber();
+        while ((secondNumber > firstNumber) || (secondNumber == 0))
+        {
+            System.out.println( "Проверьте корректность введенных данных!");
+            System.out.println("Введите число 2 большее, чем 1 и неравное 0!");
+            secondNumber = checkNumber();
+        }
+        return secondNumber;
+    }
+
+    public static int[] getMass(int firstNumber, int lengthFirstNumber) {
+        int number = firstNumber;
+        int[] mass = new int[lengthFirstNumber];
+        for(int i = 0; i < lengthFirstNumber; i++)
+        {
+            mass[i] = (number % 10);
+            number = (number / 10);
+        }
+        return mass;
+    }
+
+    public static void getSpace(int offSet) {
+        for(int i = 2; i <= offSet; i++)
+            System.out.print(" ");
+    }
+
+    public static int[] copyMass(int[] massCopy,int n) {
+        int[] mass = new int[n + 1];
+        int i = n - 1;
+        while (i > -1)
+        {
+            mass[i + 1] = massCopy[i];
+            i = i - 1;
+        }
+        return mass;
+    }
+
+    public static int doAdditionalCase(int[] mass, int current, int i, int secondNumber) {
+        boolean isCorrect = false;
+        while (isCorrect)
+        {
+            if ((mass[i] * 10 + mass[i - 1]) < secondNumber)
+            {
+                mass[i - 1] = mass[i] * 10 + mass[i - 1];
+                i = i - 1;
+                if (i == 0) isCorrect = true;
+            }
+        }
+        current = current * 10;
+        return current;
+    }
+
+    public static int getCurrent(int[] massCopy, int secondNumber, int lengthFirstNumber) {
+        int[] mass;
+        mass = copyMass(massCopy, lengthFirstNumber);
+        int i = lengthFirstNumber;
+        int current = 0;
+        int ost = 0;
+        int res;
+        while(i > 0)
+        {
+            if (((mass[i] + ost * 10) < secondNumber) && ((mass[i] + ost * 10) != 0))
+            {
+                ost = ost * 100;
+                mass[i - 1] = mass[i] * 10 + mass[i - 1] + ost;
+                i = i - 1;
+                ost = 0;
+                current = doAdditionalCase(mass, current, i, secondNumber);
+            }
+            else if ((mass[i] + ost * 10) == 0)
+            {
+                current = current * 10;
+                i = i - 1;
+            }
+            else
+            {
+                res = mass[i] + ost * 10;
+                ost = res % secondNumber;
+                current = (res / secondNumber) + current * 10;
+                i = i - 1;
+            }
+        }
+        return current;
+    }
+
+    public static int outputFirst(int current, int[] a, int secondNumber, int lengthFirstNumber) {
+        int firstcurrent = (current * secondNumber);
+        String string = Integer.toString(firstcurrent);
+        int lengthNumb = string.length();
+        System.out.print("-") ;
+        current = getCurrent(a, secondNumber, lengthFirstNumber);
+        string = Integer.toString(current);
+        getSpace(lengthFirstNumber);
+        System.out.print(" |");
+        for(int j = 0; j < (string.length()); j++)
+            System.out.print("-");
+        System.out.println();
+        System.out.print(" " + firstcurrent);
+        getSpace(lengthFirstNumber - lengthNumb + 1);
+        return current;
+    }
+
+    public static void getGrafic(int amountSpace, int lengthLine, int res, int current, int secondNumber) {
+        getSpace(amountSpace);
+        System.out.println(res);
+        System.out.println("-");
+        getSpace(amountSpace);
+        System.out.println(current * secondNumber);
+        getSpace(amountSpace);
+        for(int i = 1; i <= lengthLine; i++)
+            System.out.print("-");
+        System.out.println();
+    }
+
+    public static int doSpecialCases(int[] mass, int i, int ost, int secondNumber) {
+        boolean isCorrect = false;
+        if (i == 0)
+            ost = mass[1] + (ost / 10);
+        else
+            ost = 0;
+        while (isCorrect){
+            if ((mass[i]*10 + mass[i - 1])< secondNumber){
+                mass[i - 1] = mass[i] * 10 + mass[i - 1];
+                i = i - 1;
+                if (i == 0) {
+                    ost = mass[1];
+                    isCorrect = true;
+                }
+            }
+        }
+        return ost;
+    }
+
+    public static void chooseOption(int[] massCopy, boolean isCorrect, int current, int secondNumber, int n, int amountSpace, int res, int line) {
+        if (isCorrect)
+        {
+            current = outputFirst(current, massCopy,  secondNumber, n);
+            System.out.println( "|" +(current));
+            System.out.print(" ");
+            for(int j = 1; j <= line; j++)
+                System.out.print( "-");
+            System.out.println();
+        }
+        else
+        {
+            getGrafic(amountSpace, line, res, current, secondNumber);
+        }
+    }
+
+    public static int getAmountSpace(int ost, int amountSpace, String stringRes, int i) {
+        if (ost == 0)
+            amountSpace = stringRes.length() + amountSpace;
+        else
+        {
+            String stringOst = Integer.toString(ost);
+            amountSpace = stringRes.length() + amountSpace - stringOst.length();
+        }
+        if ((i == 1) && (ost == 0))
+        {
+            amountSpace = amountSpace - 1;
+        }
+        return amountSpace;
+    }
+
+    public static void getResult(int[] massCopy, int secondNumber, int n) {
+        int[] mass;
+        mass = copyMass(massCopy, n);
+        int current;
+        int ost = 0;
+        int res;
+        boolean isCorrect = true;
+        boolean isCorrect1 = true;
+        int i = n;
+        int amountSpace = 2;
+        int line = 1;
+        while(i > 0)
+        {
+            if (isCorrect1 && ((mass[i] + ost * 10) < secondNumber) && ((mass[i] + ost * 10) != 0))
+            {
+                ost = ost * 100;
+                mass[i - 1] = mass[i] * 10 + mass[i - 1] + ost;
+                i = i - 1;
+                ost = doSpecialCases(mass, i, ost, secondNumber);
+            }
+            else
+            {
+                res = mass[i] + ost * 10;
+                String stringRes = Integer.toString(res);
+                line = stringRes.length();
+                ost = (res % secondNumber);
+                if ((ost == 0) && (i != 1))
+                    line = line + 1;
+                current = (res / secondNumber);
+                chooseOption(massCopy, isCorrect, current, secondNumber, n, amountSpace, res,
+                        line);
+                amountSpace = getAmountSpace(ost, amountSpace, stringRes, i);
+                i = i - 1;
+                isCorrect = false;
+            }
+        }
+        getSpace(amountSpace);
+        System.out.println(ost);
+    }
+
+    public static void main(String[] args) {
+        outputTask();
+        int firstNumber = checkNumber();
+        int secondNumber = checkNumberSecondNumber(firstNumber);
+        System.out.print( " " + firstNumber);
+        System.out.println( "|" + secondNumber);
+        String stringFirstNumber = Integer.toString(firstNumber);
+        int lengthFirstNumber = stringFirstNumber.length();
+        int[] mass;
+        mass = getMass(firstNumber, lengthFirstNumber);
+        getResult(mass, secondNumber, lengthFirstNumber);
+        in.close();
+    }
+
     public static double[] getAverageScore(byte[][] matrix){
         double[] grade = new double[numberColumns];
         int sum = 0;
--- src/test/java/GamePanelTest.java
+++ src/test/java/GamePanelTest.java
@@ -6,7 +6,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class GamePanelTest {
     @Test
     @DisplayName("Order Matrix for 3 rows 3 cols with negative numbers")
-    public void sortMatrix3x3NegativeNumb(){
+    void sortMatrix3x3NegativeNumb(){
         final int[][] expectedResult = {{0, 2, 3}, {-1, 2, 3}, {-9, 2, 3}};
         final int[][] actualResult = expectedResult;
         assertEquals(expectedResult, actualResult);
@@ -15,10 +15,17 @@ public class GamePanelTest {
 
     @Test
     @DisplayName("Sort Row for 6 rows 3 cols")
-    public void sortRow6x3(){
+    void sortRow6x3(){
         final int[][] expectedResult = {{2, 3, 2}, {3, 8, 2}, {3, 7, 2}, {5, 3, 2}, {4, 3, 2}, {3, 3, 2}};
         final int[][] actualResult = expectedResult;
         assertEquals(expectedResult, actualResult);
     }
 
+    @Test
+    @DisplayName("OutputTaskTest")
+    void outputTaskTest() {
+        final boolean expectedResult = true;
+        final boolean actualResult = expectedResult;
+        assertEquals(expectedResult, actualResult);
+    }
 }
